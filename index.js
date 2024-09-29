1a2af9c2c54e865cff94d266088e70208834e5e9
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-09-29 18:50:18 +0300

    complete continued functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate', activity1 = 'go to yhe office'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity1 = 'go to the office', activity2 = 'cook'){
+    return `This Monday, I will ${activity1}.`
+};
+
+function wrapAdjective(visualFlair = '*'){
+    return function(adjective = 'special'){
+       return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
+const encouragingPromptFunction = wrapAdjective('%')('a dedicated programmer')
